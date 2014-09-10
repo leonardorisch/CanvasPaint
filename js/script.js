0d0d80c674be9a1d7f5b8a53ef94c88e95f52c88
lucashaensch <lucashaensch@gmail.com>
2014-09-09 21:14:15 -0300

    changed lotsa stuff

--- js/script.js
+++ js/script.js
@@ -29,6 +29,8 @@ function InitThis() {
             Line(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
         } else if(tool[2].checked){
             Rectangle(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        } else if(tool[3].checked){
+            Circle(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
         }
     });
     
@@ -43,25 +45,13 @@ function InitThis() {
         lineWidth.appendChild(opt);
     }
 
-    $('#colorpickerField').ColorPicker({
-        onSubmit: function(hsb, hex, rgb, el) {
-            $(el).val(hex);
-            $(el).ColorPickerHide();
-        },
-        onBeforeShow: function () {
-            $(this).ColorPickerSetColor(this.value);
-        }
-    })
-    .bind('keyup', function(){
-        $(this).ColorPickerSetColor(this.value);
-    });
 	changeCursorCrosshair();
 }
 
 function Brush(x, y, isDown) {
     if (isDown) {
         ctx.beginPath();
-        ctx.strokeStyle = '\#' + $('#colorpickerField').val();
+        ctx.strokeStyle = $('#colorpickerField').val();
         ctx.lineWidth = $('#selWidth').val();
         ctx.lineJoin = "round";
         ctx.moveTo(lastX, lastY);
@@ -72,9 +62,9 @@ function Brush(x, y, isDown) {
     lastX = x; lastY = y;
 }
 
-function Line(x, y, isDown){
+function Line(x, y){
     ctx.beginPath();
-    ctx.strokeStyle = '\#' + $('#colorpickerField').val();
+    ctx.strokeStyle = $('#colorpickerField').val();
     ctx.lineWidth = $('#selWidth').val();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(x, y);
@@ -83,18 +73,33 @@ function Line(x, y, isDown){
 
 function Rectangle(x, y){
     ctx.beginPath();
-    ctx.strokeStyle = '\#' + $('#colorpickerField').val();
+    ctx.strokeStyle = $('#colorpickerField').val();
+    ctx.fillStyle = $('#colorpickerField2').val();
     ctx.lineWidth = $('#selWidth').val();
     ctx.rect(lastX,lastY,x,y);
     ctx.stroke();
+    ctx.fill();
 }
 
-function draw(tool){
+function Circle(x, y){
+    ctx.beginPath();
+    ctx.strokeStyle = $('#colorpickerField').val();
+    ctx.fillStyle = $('#colorpickerField2').val();
+    ctx.lineWidth = $('#selWidth').val();
+    ctx.arc(lastX, lastY, getRadius(x, y, lastX, lastY), 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.fill();
+}
 
+function getRadius(x, y, lastX, lastY){
+    b = (x - lastX < 0 ? (x - lastX) * -1 : x - lastX);
+    c = (y - lastY < 0 ? (y - lastY) * -1 : y - lastY);
+    a = Math.sqrt(b*b + c*c);
+
+    return a;
 }
     
 function clearArea() {
-    // Use the identity matrix while clearing the canvas
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
